e255563fdf337008560e70a6aad3a6888254c901
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2026-08-13 20:58:12 -0300

    Adjust unlock root skills

--- neoforge-project/BHVinebound/src/main/java/io/github/antthluca/bhvinebound/serializers/custom/VineArmorData.java
+++ neoforge-project/BHVinebound/src/main/java/io/github/antthluca/bhvinebound/serializers/custom/VineArmorData.java
@@ -10,10 +10,23 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
+import net.puffish.skillsmod.api.Category;
+import net.puffish.skillsmod.api.Skill;
 import net.puffish.skillsmod.api.SkillsAPI;
+
+import java.util.List;
+import java.util.Optional;
 
 public record VineArmorData(boolean unlocked) {
     public static final Identifier VINE_ARMOR_CATEGORY = Identifier.fromNamespaceAndPath(BHVinebound.MODID, "vine_armor");
+
+    private static final List<String> ROOT_SKILLS = List.of(
+            "cpsmmy0ax7t721pa",
+            "ferds6gxxu7ejimp",
+            "bjqzpvkg7b0lpa43",
+            "f7auo06yitl8iuly",
+            "mh61pmualgb0hg37"
+    );
 
     public static final MapCodec<VineArmorData> MAP_CODEC = RecordCodecBuilder.mapCodec(instance ->
             instance.group(
@@ -37,8 +50,18 @@ public record VineArmorData(boolean unlocked) {
 
     public VineArmorData setUnlocked(Player player) {
         if (player instanceof ServerPlayer serverPlayer) {
+            Optional<Category> opCategory = SkillsAPI.getCategory(VINE_ARMOR_CATEGORY);
             SkillsAPI.getCategory(VINE_ARMOR_CATEGORY)
-                    .ifPresent(cat -> cat.unlock(serverPlayer));
+                    .ifPresent(cat -> {
+                        cat.unlock(serverPlayer);
+
+                        for (String skill : ROOT_SKILLS) {
+                            cat.getSkill(skill)
+                                    .ifPresent(skl -> skl.unlock(serverPlayer));
+                        }
+
+                        cat.openScreen(serverPlayer);
+                    });
         }
 
         return new VineArmorData(true);
